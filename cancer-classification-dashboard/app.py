9580d9f802954f8cb536a78666e940978a2b282a
Erebus2306 <kshitiz23kumar@gmail.com>
2025-03-23 14:40:35 +0530

    Removed

--- cancer-classification-dashboard/app.py
+++ cancer-classification-dashboard/app.py
@@ -69,8 +69,8 @@ if page == "Introduction":
     Use the sidebar on the left to navigate between different sections of the dashboard.
     """)
     
-    st.image("https://www.theguardian.com/society/2022/oct/29/tiny-endo-microscope-could-spot-breast-cancer-cells-forming#img-1", 
-             caption="Metastasized breast cancer cells under microscope. A tiny endo-microscope being developed at Imperial College London is hoped to help surgeons identify cancerous cells much more quickly. Credit: Cultura Creative Ltd/Alamy")
+    """st.image("https://www.theguardian.com/society/2022/oct/29/tiny-endo-microscope-could-spot-breast-cancer-cells-forming#img-1", 
+             caption="Metastasized breast cancer cells under microscope. A tiny endo-microscope being developed at Imperial College London is hoped to help surgeons identify cancerous cells much more quickly. Credit: Cultura Creative Ltd/Alamy")"""
 
 elif page == "Data Explorer":
     st.title("Data Explorer")
